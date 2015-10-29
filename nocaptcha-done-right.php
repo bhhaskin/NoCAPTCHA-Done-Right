a507c493ccdb649657c8544bc3ec322ff4ef47e7
Bryan Haskin <bhhaskin@gmail.com>
2015-10-29 14:59:59 -0700

    inital commit

---
+++ nocaptcha-done-right.php
@@ -0,0 +1,17 @@
+/**
+ * @package   Nocaptcha_Done_Right
+ * @author    Bryan Haskin <bhhaskin@gmail.com>
+ * @license   MIT
+ * @link      https://bryans.website
+ * @copyright 2015 Bryan Haskin
+ *
+ * Plugin Name:       NoCAPTCHA Done Right
+ * Description:
+ * Version:           0.1
+ * Author:            Bryan Haskin
+ * Author URI:        https://bryans.website
+ * Text Domain:       google-nocaptcha-recaptcha-locale
+ * License:           MIT
+ * License URI:       https://opensource.org/licenses/MIT
+ * Domain Path:       /languages
+ */
